5bf91a8a73ada9cda31c4c7e75da42f397759139
Sanket <msanket450@gmail.com>
2022-07-10 10:40:05 +0530

    Reverse an given integer

---
+++ 01_Problems/13_reverse_integer.cpp
@@ -0,0 +1,18 @@
+# include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<< "Enter n = ";
+    cin>> n;
+
+    int ans = 0;
+    while(n != 0) {
+        int digit = n % 10;
+        ans = (ans * 10) + digit;
+        n = n / 10;
+    }
+
+    cout<< ans << endl;
+    return 0;
+}

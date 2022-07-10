6b789b24a1f768e7393b2a9c7186eab43e3b71e2
Sanket <msanket450@gmail.com>
2022-07-10 11:01:06 +0530

    Transpose of given matrix

--- Matrix Problems/matrix_transpose.cpp
+++ Matrix Problems/matrix_transpose.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main(){
+    // Given 2-D matrix
     int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
+    // Logic for transpose of matrix
     for(int i = 0;i < 3;i++){
         for(int j = i;j < 3;j++) {
             int temp = arr[i][j];
@@ -12,6 +14,7 @@ int main(){
         }
     }
 
+    // Printing the matrix
     for(int i = 0;i < 3;i++) {
         for(int j = 0;j < 3;j++) {
             cout<< arr[i][j] << " ";

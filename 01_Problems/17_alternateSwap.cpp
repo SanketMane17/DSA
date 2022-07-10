20edfc4f011659c15da233214b47909243065360
Sanket <msanket450@gmail.com>
2022-07-10 10:41:46 +0530

    Swap alternate from an array

---
+++ 01_Problems/17_alternateSwap.cpp
@@ -0,0 +1,30 @@
+# include<iostream>
+using namespace std;
+
+void alternateSwap(int *arr, int size) {
+    int pos1 = 0; 
+    int pos2 = 1;
+    
+    while(pos2 <+ size) {
+        swap(arr[pos1], arr[pos2]);
+        pos1 += 2;
+        pos2 += 2;
+    }
+}
+
+void printArray(int *arr, int n) {
+    for(int i = 0;i < n;i++) {
+        cout<< arr[i] << " ";
+    }
+}
+
+int main(){
+    int arr[] = {2, 1, 8, 7, 5, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    printArray(arr, size);
+    cout<< endl;
+    alternateSwap(arr, size);
+    printArray(arr, size);
+    return 0;
+}

8eb75a0ebd5666c546d3eb19bf73393ee05720cd
Sanket <msanket450@gmail.com>
2022-09-17 11:01:31 +0530

    C++ code

--- 01_Problems/77_maximum_product_subarray.cpp
+++ 01_Problems/77_maximum_product_subarray.cpp
@@ -6,32 +6,51 @@
 // Output: 0
 // Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 
-# include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int maxProduct(vector<int> nums) {
+int maxProduct(vector<int> nums)
+{
+
+    int n = nums.size();
+    int ans = nums[0];
+    int ma = ans;
+    int mi = ans;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (nums[i] < 0)
+            swap(ma, mi);
+
+        ma = max(nums[i], ma * nums[i]);
+        mi = min(nums[i], mi * nums[i]);
+        ans = max(ans, ma);
+    }
+    return ans;
 
     // TC - O(n^2)
     // SC - O(1)
     // Given TLE --> Not efficient
-    
-    int n = nums.size();
-    if(n == 1)
-        return nums[0];
-    int maxProduct = INT_MIN;
-    
-    for(int i = 0;i < n;i++) {
-        int currProduct = 1;
 
-        for(int j = i;j < n;j++) {
-            currProduct *= nums[j];
-            maxProduct = max(maxProduct, currProduct);
-        }
-    }
-    return maxProduct;
+    // if(n == 1)
+    //     return nums[0];
+    // int maxProduct = INT_MIN;
+
+    // for(int i = 0;i < n;i++) {
+    //     int currProduct = 1;
+
+    //     for(int j = i;j < n;j++) {
+    //         currProduct *= nums[j];
+    //         maxProduct = max(maxProduct, currProduct);
+    //     }
+    // }
+    // return maxProduct;
 }
 
-int main(){
-    
+int main()
+{
+
+    vector<int> nums = {2, 3, -2, 4};
+    cout << maxProduct(nums) << endl;
     return 0;
 }

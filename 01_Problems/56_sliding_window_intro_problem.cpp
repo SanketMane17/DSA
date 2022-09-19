2791d68076d9a7789dd7a586d26411e6b2bc34ef
Sanket <msanket450@gmail.com>
2022-09-19 12:59:40 +0530

    C++ code

--- 01_Problems/56_sliding_window_intro_problem.cpp
+++ 01_Problems/56_sliding_window_intro_problem.cpp
@@ -1,40 +1,47 @@
-# include<iostream>
-# include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int findSum(vector<int> & arr) {
+int findSum(vector<int> &arr)
+{
 
     int sum = 0;
-    for(int i = 0;i < arr.size();i++) {
+    for (int i = 0; i < arr.size(); i++)
+    {
         sum += arr[i];
     }
     return sum;
 }
 
-int findMaxSubarraySumOfK(vector<int> & arr, int n, int k) {
+int findMaxSubarraySumOfK(vector<int> &arr, int n, int k)
+{
 
-    int sum = 0;
-    int maxSum = INT_MIN;
+    // TC - O(n)
+    // SC - O(1)
 
-    // First window
-    
-    for(int i = 0;i < k;i++) {
-        sum += arr[i];
-    }
+    // Approach - 1
+    // int sum = 0;
+    // int maxSum = INT_MIN;
 
-    maxSum = max(sum, maxSum);
+    // // First window
+    // for(int i = 0;i < k;i++) {
+    //     sum += arr[i];
+    // }
 
-    cout<< sum << endl;
-    for(int i = k ;i < n;i++) {        
-        sum += arr[i] - arr[i - k];
-        maxSum = max(sum, maxSum);
-    }
-    return maxSum;
-    
+    // maxSum = max(sum, maxSum);
+
+    // for(int i = k ;i < n;i++) {
+    //     sum += arr[i] - arr[i - k];
+    //     maxSum = max(sum, maxSum);
+    // }
+    // return maxSum;
+
+    // Approach - 1
+
     // First window
     // int i;
     // for(i = 0;i < k;i++) {
-        // sum += arr[i];
+    // sum += arr[i];
     // }
     // int right = i, left;
 
@@ -47,20 +54,36 @@ int findMaxSubarraySumOfK(vector<int> & arr, int n, int k) {
     //         maxSum = sum;
     //     right++;
     // }
+
+    // TC - O(n * k)
+    // SC - O(1)
+
+    // int maxSum = INT_MIN;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int sum = 0;
+    //     for (int j = 0; j < k; j++)
+    //     {
+    //         sum += arr[i + j];
+    //     }
+    //     maxSum = max(sum, maxSum);
+    // }
+    // return maxSum;
 }
 
-int main(){
-    
+int main()
+{
+
     // vector<int> arr = {2, 6, 0, 10,1};
     // vector<int> arr = {100, 200, 300, 400};
-    vector<int> arr = {807, 249, 73, 658, 930, 272, 544, 878, 923, 709, 440, 165, 492, 42, 987, 503, 327, 729, 840, 612, 303, 169, 709, 157, 560, 933, 99, 278, 816, 335, 97, 826, 512, 267, 810, 633, 979, 149, 579, 821, 967, 672, 393, 336, 485, 745, 228, 91, 194, 357, 1 ,153, 708}; // 25834
+    vector<int> arr = {807, 249, 73, 658, 930, 272, 544, 878, 923, 709, 440, 165, 492, 42, 987, 503, 327, 729, 840, 612, 303, 169, 709, 157, 560, 933, 99, 278, 816, 335, 97, 826, 512, 267, 810, 633, 979, 149, 579, 821, 967, 672, 393, 336, 485, 745, 228, 91, 194, 357, 1 ,153, 708}; // 25834 , k = 52
     int n = arr.size();
     int k = 52;
 
     // cout<< findSum(arr) << endl;
 
     int res = findMaxSubarraySumOfK(arr, n, k);
-    cout<< res << endl;
+    cout << res << endl;
 
     return 0;
 }
--- 01_Problems/57_largest_subarray_sum_0.cpp
+++ 01_Problems/57_largest_subarray_sum_0.cpp
@@ -5,62 +5,109 @@
 // Explanation
 // The largest subarray with sum zero is (3, 1, -4, 0)
 
-# include<iostream>
-# include<vector>
-# include<unordered_map>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-vector<int> largestSubarraySumZero(int n, vector<int> arr){
+vector<int> largestSubarraySumZero(int n, vector<int> arr)
+{
 
     int sum = 0;
     unordered_map<int, int> mpp;
-    int maxLen = -1, start , end, len;
-    
-    // Edge case for first element as 0
-    if(arr[0] == 0)
-        mpp[0] = -1;
+    int maxLen = -1, start, end, len;
 
-    for(int i = 0;i < n;i++) {
+    // Edge case for 0
+    mpp[0] = -1;
+
+    for (int i = 0; i < n; i++)
+    {
         sum += arr[i];
 
-        if(mpp.find(sum) != mpp.end()) {
+        if (mpp.find(sum) != mpp.end())
+        {
+            // cout<< i << " " << mpp[sum] << endl;
             len = i - mpp[sum];
-            
-            if(len > maxLen) {
+
+            if (len > maxLen)
+            {
                 end = i;
                 start = mpp[sum];
                 maxLen = len;
             }
-        } else {
+        }
+        else
+        {
             mpp[sum] = i;
         }
     }
 
+    for (auto it : mpp)
+    {
+        cout << it.first << " : " << it.second << endl;
+    }
+
     // Return -1 if not found any subarray
-    if(maxLen == 0)
+    if (maxLen == 0)
         return {-1};
 
     vector<int> ans;
-    for(int i = start + 1 ;i <= end;i++) {
+    for (int i = start + 1; i <= end; i++)
+    {
         ans.push_back(arr[i]);
     }
     return ans;
+
+    // Brute force
+    // TC - O(n^2)
+    // SC - O(n)
+
+    // int maxLen = INT_MIN;
+    // int start, end;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int len = 0, sum = 0;
+    //     for (int j = i; j < n; j++)
+    //     {
+    //         sum += arr[j];
+    //         if (sum == 0)
+    //         {
+    //             len = j - i + 1;
+    //             if(len > maxLen) {
+    //                 start = i;
+    //                 end = j;
+    //                 maxLen = len;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // cout << start << " " << end<< endl;
+    // vector<int> ans;
+    // for(int i = start;i <= end;i++) {
+    //     ans.push_back(arr[i]);
+    // }
+    // return ans;
 }
 
-int main(){
+int main()
+{
     // vector<int> arr = {2, 3, 1, -4, 0, 6};
+    // vector<int> arr = {0, 1, 2, 3, -1, 1};
+    vector<int> arr = {-1, 1, -1, 1};
     // vector<int> arr = {1, 0, 0, 2, 3};
     // vector<int> arr = {1, 2, -3, 3};
     // vector<int> arr = {1, -1, 3, 2, -2, -8, 1, 7, 10, 23, 1, -1, -2, 2, -3, 3, -4, 2, 2, 5, -3, -2};
 
-    vector<int> arr = {17, -20, 3, 8, -5, -4, 12, 19, 9, -12, -11, 3, 18, -15, 15, -1, 20, 3, -19, 6, 6, -20, -10, 11, -14, -12, -9, -6, -18, -8, 17, 6, -18, -1, -4, 10, 9, 2, 14, 9, 4, 5, 15, 17, -7, -7, -2, 10, -9, 4, 14, -17, -3, -15, -20, -12, -18, -9, -4, -9, 10, 0, -5, 19, -5, 12, 0, -10, 15, -2, -3, 16, -1, 12, 13, -1, -19, 5, 16, 17, -17, -14, -20, -19, 15, -2, -4, 0, 5, 6, 18, -5, -15, 10, 6};
+    // vector<int> arr = {17, -20, 3, 8, -5, -4, 12, 19, 9, -12, -11, 3, 18, -15, 15, -1, 20, 3, -19, 6, 6, -20, -10, 11, -14, -12, -9, -6, -18, -8, 17, 6, -18, -1, -4, 10, 9, 2, 14, 9, 4, 5, 15, 17, -7, -7, -2, 10, -9, 4, 14, -17, -3, -15, -20, -12, -18, -9, -4, -9, 10, 0, -5, 19, -5, 12, 0, -10, 15, -2, -3, 16, -1, 12, 13, -1, -19, 5, 16, 17, -17, -14, -20, -19, 15, -2, -4, 0, 5, 6, 18, -5, -15, 10, 6};
 
     int n = arr.size();
-    cout<< n << endl;
+    // cout<< n << endl;
 
     vector<int> ans = largestSubarraySumZero(n, arr);
-    for(int i = 0;i < ans.size();i++) {
-        cout<< ans[i] << " ";
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
     }
     return 0;
 }
